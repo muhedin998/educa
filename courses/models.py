abdfacb39470cdacb6892dfc2f07b19538b5fae9
muhedin998 <muhedin998@gmail.com>
2022-10-02 18:31:16 +0200

    Add Content, items models

--- courses/models.py
+++ courses/models.py
@@ -51,4 +51,28 @@ class Content(models.Model):
                                       on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type','object_id')
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related',
+                              on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_add_now=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
     
+    def __str__(self):
+        return self.title
+
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField()
