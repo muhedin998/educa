85aa670e54cf15e7df6b4beba58430aab56a11b1
muhedin998 <muhedin998@gmail.com>
2022-10-03 16:45:05 +0200

    Module Editing/Creating/Deleting for Instructors

--- educa/urls.py
+++ educa/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('courses.urls')),
-    path('account/login/', auth_views.LoginView.as_view(), name='login'),
-    path('account/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('course/',include('courses.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     
 ]
