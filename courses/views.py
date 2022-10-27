26942870299f823e68d7d64c71c704f38d170cb5
muhedin998 <muhedin1998@gmail.com>
2022-10-28 00:58:11 +0200

    Implamentation of Create and Update class for Cont

--- courses/views.py
+++ courses/views.py
@@ -1,13 +1,39 @@
+from ast import Mod
+from types import NoneType
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Course
+from .models import Course, Module
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import TemplateResponseMixin, View
 from .forms import ModuleFormSet
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView,DeleteView
+from django.forms.models import modelform_factory
+from django.apps import apps
+
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = None
+    obj = None
+    template_name: 'courses/manage/content/form.html'
+
+    def get_model(self, model_name):
+        if model_name in ['text','video','image','fille']:
+            return apps.get_model(app_label='courses', model_name=model_name)
+        return None
+
+    def get_form(self, model, *args, **kwargs):
+        Form  = modelform_factory(model, exclude=['owner','order', 'created', 'updated'])
+        return Form(*args, **kwargs)
+    
+    def dispatch(self, request, module_id, model_name, id=None):     
+        self.module = get_object_or_404(Module,id=module_id, course__owner=request.user)
+        self.model = self.get_model(model_name)
+        if id:
+            self.obj = get_object_or_404(Module,id=module_id,course__owner=request.user)
+        return super().dispatch(request,module_id,model_name,id)
 
 class OwnerMixin(object):
     def get_queryset(self):
